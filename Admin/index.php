04a9a8e0410a0fd98858209fc4f42863ae804149
Vivek7282 <82724500+Vivek7282@users.noreply.github.com>
2022-06-20 21:29:55 +0530

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -117,7 +117,9 @@
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
+  <!-- Template Main JS File 
+
+-->
   <script src="assets/js/main.js"></script>
 
 </body>
